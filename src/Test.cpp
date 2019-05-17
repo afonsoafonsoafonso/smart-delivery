09d3368ee7cf94e748498185e184187f6c7eb7a7
gmtorres <torresgustam@gmail.com>
2019-05-17 01:10:53 +0100

    Added graph classes

--- src/Test.cpp
+++ src/Test.cpp
@@ -3,6 +3,14 @@
 #include "xml_listener.h"
 #include "cute_runner.h"
 
+
+#include <cstdio>
+#include "graphviewer.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+
 void thisIsATest() {
 	ASSERTM("start writing tests", false);	
 }
@@ -19,5 +27,19 @@ bool runAllTests(int argc, char const *argv[]) {
 }
 
 int main(int argc, char const *argv[]) {
-    return runAllTests(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
+    //return runAllTests(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
+
+	GraphViewer *gv = new GraphViewer(600, 600, true);
+	gv->createWindow(600, 600);
+	gv->defineVertexColor("blue");
+	gv->defineEdgeColor("black");
+	gv->addNode(1);
+	gv->addNode(2);
+	gv->addEdge(1,1,2,EdgeType::UNDIRECTED);
+	gv->rearrange();
+
+	getchar();
+
+	return 0;
 }
+
